afd14a49f27a0e807e8027d43f511cfbc244d02a
Ivan Liutko <ivanliutko@gmail.com>
2018-01-23 00:30:55 +0200

    added slider component via cli

--- CarService.Client/src/app/app.module.ts
+++ CarService.Client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
