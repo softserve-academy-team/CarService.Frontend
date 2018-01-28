ef9c194a85517bbac36986939e0bb87ba80ddf83
LytvyniukDima <33831158+LytvyniukDima@users.noreply.github.com>
2018-01-28 23:26:11 +0200

    Revert "Car List"

--- CarService.Client/src/app/app-routing.module.ts
+++ CarService.Client/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { CarListComponent } from './components/car-list/car-list.component'
 
-const routes: Routes = [
-  { path: '', component: CarouselComponent },
-  { path: 'carlist', component: CarListComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
