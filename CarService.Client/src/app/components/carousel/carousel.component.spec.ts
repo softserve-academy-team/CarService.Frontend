dbc2604dc15ea794ed18c548bb08a86b93911193
Ivan Liutko <ivanliutko@gmail.com>
2018-01-29 02:38:51 +0200

    added id attribute to buttons in order to have references when testing and wrote tests

--- CarService.Client/src/app/components/carousel/carousel.component.spec.ts
+++ CarService.Client/src/app/components/carousel/carousel.component.spec.ts
@@ -6,24 +6,25 @@ import { AboutComponent } from '../about/about.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 
-describe('BannerComponent (inline template)', () => {
+describe('CarouselComponent (inline template)', () => {
 
     let comp: CarouselComponent;
     let fixture: ComponentFixture<CarouselComponent>;
     let deBtns: DebugElement[];
+    let image: HTMLElement;
     let deContainer: DebugElement;
     let btn1: HTMLElement;
     let btn2: HTMLElement;
     let carouselContainer: HTMLElement;
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [ CarouselComponent, AboutComponent ], // declare the test component
+        declarations: [ CarouselComponent, AboutComponent ],
         imports: [MatCardModule, MatDividerModule]
       });
       fixture = TestBed.createComponent(CarouselComponent);
-      comp = fixture.componentInstance; // BannerComponent test instance
-      // query for the title <h1> by CSS element selector
+      comp = fixture.componentInstance;
       deBtns = fixture.debugElement.queryAll(By.css('button'));
+      image = fixture.debugElement.query(By.css('img')).nativeElement;
       deContainer = fixture.debugElement.query(By.css('mat-card'));
       btn1 = deBtns[0].nativeElement;
       btn2 = deBtns[1].nativeElement;
@@ -34,9 +35,41 @@ describe('BannerComponent (inline template)', () => {
       deContainer.triggerEventHandler('mouseenter', null);
       fixture.detectChanges();
       expect(comp.hide).toBe(false);
-      console.log('hide =', comp.hide);
       expect(btn1.style.visibility).toBe('visible');
-      console.log('(btn1.style.visibility=', btn1.style.visibility);
+      expect(btn2.style.visibility).toBe('visible');
   });
+    it('should call leftClick() when button with id lftBtn is clicked', () => {
+      const spy = spyOn(comp, 'leftClick');
+      // de for left button
+      deBtns[0].triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(spy).toHaveBeenCalled();
+  });
+    it('should call rightClick() when button with id rtBtn is clicked', () => {
+      const spy = spyOn(comp, 'rightClick');
+      // de for right button
+      deBtns[1].triggerEventHandler('click', null);
+      fixture.detectChanges();
+      expect(spy).toHaveBeenCalled();
+  });
+    it(`should be so that src of the image is numOfCurrImg of images array
+     when button with id rtBtn is clicked`, () => {
+      // de for right button
+      deBtns[1].triggerEventHandler('click', null);
+      console.log(deBtns[1].nativeElement.id);
+      fixture.detectChanges();
+      expect(image.attributes[3].value)
+      .toBe(comp.images[comp.numOfCurrImg].src);
+  });
+    it(`should be so that src of the image is numOfCurrImg of images array
+     when button with id lftBtn is clicked`, () => {
+      // de for left button
+      deBtns[0].triggerEventHandler('click', null);
+      console.log(deBtns[0].nativeElement.id);
+      fixture.detectChanges();
+      expect(image.attributes[3].value)
+      .toBe(comp.images[comp.numOfCurrImg].src);
+  });
+
 
 });
