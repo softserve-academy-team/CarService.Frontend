72545c9a8c8f3dbfc8394b4a65de5903100b74cb
LytvyniukDima <dima.lytvyniuk@gmail.com>
2018-03-02 04:03:38 +0200

    Change video player position

--- CarService.Client/src/app/services/auth.service.ts
+++ CarService.Client/src/app/services/auth.service.ts
@@ -4,19 +4,21 @@ import { RestUrlBuilder } from './rest-url-builder';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+declare let require: any;
+
 @Injectable()
 export class AuthService {
-
   private readonly carServiceApiBaseUrl: string;
+  private decode = require('jwt-decode');
 
-  constructor(private http: HttpClient, 
-              private restUrlBuilder: RestUrlBuilder,
-              private router: Router) {
+  constructor(private http: HttpClient,
+    private restUrlBuilder: RestUrlBuilder,
+    private router: Router) {
     this.carServiceApiBaseUrl = environment['CarServiceApiBaseUrl'];
   }
 
   signIn(credentials) {
-   return this.http.post<any>(this.restUrlBuilder.build(this.carServiceApiBaseUrl, 'account', 'token'), credentials);
+    return this.http.post<any>(this.restUrlBuilder.build(this.carServiceApiBaseUrl, 'account', 'token'), credentials);
   }
 
   logout() {
@@ -24,7 +26,16 @@ export class AuthService {
     this.router.navigate(['']);
   }
 
-  get isAuthentificated(){
+  get isAuthentificated() {
     return !!localStorage.getItem('token');
+  }
+
+  get isMechanic() {
+    if (!this.isAuthentificated)
+      return false;
+      
+    var decoded = this.decode(localStorage.getItem('token'));
+    var tokenMap = new Map(Object.entries(decoded));
+    return tokenMap.get("http://schemas.microsoft.com/ws/2008/06/identity/claims/role").toString() === "mechanic";
   }
 }
